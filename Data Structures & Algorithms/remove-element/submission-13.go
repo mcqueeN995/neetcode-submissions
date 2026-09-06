b9e999b95253f59efffec5521ca9edcc18919f5f
mcqueeN <kirill2000firsov@mail.ru>
2026-09-06 17:21:29 +0300

    Add: remove-element - submission-13

---
+++ Data Structures & Algorithms/remove-element/submission-13.go
@@ -0,0 +1,16 @@
+func removeElement(nums []int, val int) int {
+	r := len(nums) - 1
+	for r >= 0 && nums[r] == val { 
+		r--
+	}
+	for l := 0; l <= r; l++ {
+		if nums[l] == val {
+			nums[l], nums[r] = nums[r], nums[l]
+			r--
+			for r >= 0 && nums[r] == val { 
+				r--
+			}
+		}
+	}
+	return r + 1 
+}
